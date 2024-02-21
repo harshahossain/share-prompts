275b54ef2a808fd435af7af6ab48c40f74536998
harshahossain <harshahossain@gmail.com>
2024-02-21 18:56:26 +0600

    updated

---
+++ app/posts/new/page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function page() {
+  return <div>from /posts/neww</div>;
+}
---
+++ app/posts/page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function page() {
+  return <div>all post from '/posts' route</div>;
+}
